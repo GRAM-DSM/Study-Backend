f0b36a57e422b867ecff9ace990b830833d2092d
junkiYoon <67030197+junkiYoon@users.noreply.github.com>
2020-10-15 08:21:54 +0900

    Update BulletinBoard.py

--- 6기-윤준기/BulletinBoard/BulletinBoard.py
+++ 6기-윤준기/BulletinBoard/BulletinBoard.py
@@ -1,23 +1,22 @@
-class BulletinBoardFunc:
+class BulletinBoard:
     def __init__(self):
         self.cnt = 0
         self.title = []
         self.content = []
 
-    def PostArticle(self):
+    def post_article(self):
         self.title.insert(self.cnt, input('제목 : '))
         self.content.insert(self.cnt, input('내용 : '))
         self.cnt += 1
 
-    def PrintList(self):
+    def print_list(self):
 
         for a in range(0, self.cnt):
             print('------------------')
             print(self.title[a])
-            print(self.content[a])
             print('------------------')
 
-    def DeleteArticle(self):
+    def delete_article(self):
         DelTitle = input('삭제할 글의 제목을 입력하세요 : ')
         if DelTitle in self.title:
             index = self.title.index(DelTitle)
@@ -28,18 +27,18 @@ class BulletinBoardFunc:
             print('해당 제목의 글이 없습니다.')
 
 
-BBF = BulletinBoardFunc()
+BBF = BulletinBoard()
 while True:
     print("----------------------------")
     print("\t글 추가 : 1\n\t글 목록 : 2\n\t글 삭제 : 3\n\t종료 : Q")
     print("----------------------------")
     option = input("선택할 옵션을 고르세요 : ")
     if option == '1':
-        BBF.PostArticle()
+        BBF.post_article()
     elif option == '2':
-        BBF.PrintList()
+        BBF.print_list()
     elif option == '3':
-        BBF.DeleteArticle()
+        BBF.delete_article()
     elif option == 'Q' or option == 'q':
         break
     else:
